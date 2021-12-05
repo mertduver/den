4607cfe29fe939ccca3a7070c63aa0758066fa9a
Mert Düver <mmert1505@gmail.com>
2021-12-05 20:56:27 +0300

    a

--- main.c
+++ main.c
@@ -4,10 +4,26 @@
 #include<stdio.h>
 #include<string.h>
 #include<semaphore.h>
+#include <time.h>
 
-#define THREAD_COUNT 2
+
+#define THREAD_COUNT 20
+#define randnum(min, max) ((rand() % (int) (((max) + 1) - (min))) + (min))
+//usage: randnum(min int, max int));
+
+
+struct patientStruct {
+    int id;
+    int disease;//0:medicine, 1:surgery
+    float bill;
+    int hungerMeter;
+    int restroomMeter;
+};
 
 sem_t semRegistration;
+sem_t semRestroom;
+sem_t semCafe;
+sem_t semGP;
 
 //# The number of restrooms that are available.
 int RESTROOM_SIZE = 10;
@@ -25,6 +41,17 @@ int BLOOD_LAB_NUMBER = 10;
 int OR_NUMBER = 10;
 int SURGEON_NUMBER = 30;
 int NURSE_NUMBER = 30;
+
+//The maximum number of surgeons and nurses that can do a surgery. A random value is
+//calculated for each operation between 1 and given values to determine the required
+//number of surgeons and nurses.
+int SURGEON_LIMIT = 5;
+int NURSE_LIMIT = 5;
+
+// The number of patients that will be generated over the course of this program.
+int PATIENT_NUMBER = 1000;
+// The account of hospital where the money acquired from patients are stored.
+int HOSPITAL_WALLET = 0;
 
 int WAIT_TIME = 100;
 int REGISTRATION_TIME = 100;
@@ -46,5 +73,60 @@ int CAFE_COST = 200; // Calculated randomly between 1 and given value.
 int HUNGER_INCREASE_RATE = 10;
 int RESTROOM_INCREASE_RATE = 10;
 
+void *patient(void *arg)
+{
+    struct patientStruct* p= (struct patientStruct*) arg;
+    printf("id:%d",p->id);
+    printf("hungerMeter:%d",p->hungerMeter);
 
+    sem_wait(&semRegistration);
+    printf("patient%s Entered the registration office\n",p->id);
+    sleep(randnum(1,REGISTRATION_TIME/10));
+    sem_post(semRegistration);
+
+    sem_wait(semGP);
+    printf("patient%s Entered the GP\n",p->id);
+    sleep(randnum(1,GP_TIME/10));
+    sem_post(&semGP);
+}
+
+
+int main()
+{
+
+    srand(time(NULL));
+    //usage: randnum(min int, max int));
+
+
+    int err;
+    int value;
+
+    sem_init(&semRegistration, 0, REGISTRATION_SIZE);
+    sem_init(&semGP, 0, GP_NUMBER);
+
+    struct patientStruct *patients[PATIENT_NUMBER];
+
+    for (int i = 0; i < PATIENT_NUMBER; ++i) {
+        patients[i]->id=i;
+        patients[i]->disease= randnum(0,1);
+        patients[i]->hungerMeter= randnum(1,100);
+        patients[i]->restroomMeter= randnum(1,100);
+        err=pthread_create(i,NULL,patient,(void *)patients[i]);
+        if (err != 0)
+            printf("Thread creation error: [%s]",
+                   strerror(err));
+    }
+
+
+    for (int i = 0; i < PATIENT_NUMBER; ++i) {
+        pthread_join(patients[i],NULL);
+    }
+
+    sem_getvalue(&semRegistration, &value);
+    printf("\n");
+    printf("Main thread: Final value of the semaphore registration is %d\n\n", value);
+    sem_destroy(&semRegistration);
+
+    return 0;
+}
 
