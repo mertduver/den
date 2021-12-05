88d7de21e3a407ad9d0ef3cc92643eeec9b089ae
medor_vm_mint <mmert1505@gmail.com>
2021-12-05 19:38:38 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/den.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/den.dir/main.c.o"
+  "den"
+  "den.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/den.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,39 @@
+//# The number of registration desks that are available.
+int REGISTRATION_SIZE = 10;
+//# The number of restrooms that are available.
+int RESTROOM_SIZE = 10;
+//# The number of cashiers in cafe that are available.
+int CAFE_NUMBER = 10;
+//# The number of General Practitioner that are available.
+int GP_NUMBER = 10;
+//# The number of cashiers in pharmacy that are available.
+int PHARMACY_NUMBER = 10;
+//# The number of assistants in blood lab that are available.
+int BLOOD_LAB_NUMBER = 10;
+//The number of operating rooms, surgeons and nurses that are available.
+int OR_NUMBER = 10;
+int SURGEON_NUMBER = 30;
+int NURSE_NUMBER = 30;
+
+int WAIT_TIME = 100;
+int REGISTRATION_TIME = 100;
+int GP_TIME = 200;
+int PHARMACY_TIME = 100;
+int BLOOD_LAB_TIME = 200;
+int SURGERY_TIME = 500;
+int CAFE_TIME = 100;
+int RESTROOM_TIME = 100;
+
+int REGISTRATION_COST = 100;
+int PHARMACY_COST = 200; // Calculated randomly between 1 and given value.
+int BLOOD_LAB_COST = 200;
+int SURGERY_OR_COST = 200;
+int SURGERY_SURGEON_COST = 100;
+int SURGERY_NURSE_COST = 50;
+int CAFE_COST = 200; // Calculated randomly between 1 and given value.
+
+int HUNGER_INCREASE_RATE = 10;
+int RESTROOM_INCREASE_RATE = 10;
+
+
+
